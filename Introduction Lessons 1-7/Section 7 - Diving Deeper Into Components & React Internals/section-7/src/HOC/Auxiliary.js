1de9be087a0c5f831d5ea9b19d1a10a392a2a8b9
Yudhishthir Singh <singh.yudi10@gmail.com>
2019-10-26 23:39:04 +0530

    HOC

---
+++ Introduction Lessons 1-7/Section 7 - Diving Deeper Into Components & React Internals/section-7/src/HOC/Auxiliary.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const aux = props => props.children;
+
+export default aux;
--- Introduction Lessons 1-7/Section 7 - Diving Deeper Into Components & React Internals/section-7/src/components/Persons/Person/Person.js
+++ Introduction Lessons 1-7/Section 7 - Diving Deeper Into Components & React Internals/section-7/src/components/Persons/Person/Person.js
@@ -1,20 +1,23 @@
 import React, { Component } from "react";
 import "./Person.css";
+import Aux from "../../../HOC/Auxiliary";
 
 class Person extends Component {
   render() {
     return (
-      <div className="Person">
+      <Aux>
+        {/* // <div className="Person"> */}
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old
         </p>
-        <p>{this.props.children}</p>
+        <p>{this.props.children}</p>,
         <input
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
         />
-      </div>
+      </Aux>
+      // {/* </div> */}
     );
   }
 }
