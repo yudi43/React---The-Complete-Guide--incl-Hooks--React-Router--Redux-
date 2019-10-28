bb2c19c61d3851d032c7ec7b38006380e04c164b
Yudhishthir Singh <singh.yudi10@gmail.com>
2019-10-29 01:58:59 +0530

    Updated burger updation

--- Section 8 - A Real App - The Burger Builder (Basic Version)/the-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ Section 8 - A Real App - The Burger Builder (Basic Version)/the-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,10 +9,10 @@ class BurgerBuilder extends React.Component {
   //   }
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 2,
-      meat: 2
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
     }
   };
 
