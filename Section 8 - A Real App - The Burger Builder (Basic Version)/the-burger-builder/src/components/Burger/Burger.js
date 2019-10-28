aee545f1bbeef519e6b07256f48ef4ee8b4b3de8
Yudhishthir Singh <singh.yudi10@gmail.com>
2019-10-29 01:40:45 +0530

    Dynamic Burger

--- Section 8 - A Real App - The Burger Builder (Basic Version)/the-burger-builder/src/components/Burger/Burger.js
+++ Section 8 - A Real App - The Burger Builder (Basic Version)/the-burger-builder/src/components/Burger/Burger.js
@@ -1,12 +1,19 @@
 import React from "react";
 import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
 const burger = props => {
+  //here we are taking the object of ingredients from props.ingredients and we want to output an array of <BurgerIngredients /> component.
+  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient key={igKey + i} type={igKey} />;
+    });
+  });
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- Section 8 - A Real App - The Burger Builder (Basic Version)/the-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ Section 8 - A Real App - The Burger Builder (Basic Version)/the-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,11 +3,24 @@ import Aux from "../../hoc/Auxiliary";
 import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends React.Component {
+  //   constructor(props) {
+  //     super(props);
+  //     this.state = {};
+  //   }
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+    }
+  };
+
   render() {
     return (
       <Aux>
         <div>
-          <Burger />
+          <Burger ingredients={this.state.ingredients} />
         </div>
         <div>Build Controls</div>
       </Aux>
