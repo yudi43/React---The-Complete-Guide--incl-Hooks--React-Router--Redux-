43d2d13d6c751ec5b4d7855052ccb37a2b2c6aa2
Yudhishthir Singh <singh.yudi10@gmail.com>
2019-10-31 01:57:15 +0530

    Added logo and toolbar

--- Section 8 - A Real App - The Burger Builder (Basic Version)/the-burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ Section 8 - A Real App - The Burger Builder (Basic Version)/the-burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import classes from "./Toolbar.module.css";
+import Logo from "../../Logo/Logo";
+
 const toolbar = props => {
   return (
     <header className={classes.Toolbar}>
       <div>MENU</div>
-      <div>LOGO</div>
+      <Logo />
       <nav>
         {/* <ul></ul> */}
         ...
