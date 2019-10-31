aaf7bb3fb0622a37f2e5e25fcdad4ec4b4d628bc
Yudhishthir Singh <singh.yudi10@gmail.com>
2019-11-01 04:18:05 +0530

    dynamic stuff

--- Section 9 - Reaching out to the web (Http, Ajax)/http-01/src/components/Post/Post.js
+++ Section 9 - Reaching out to the web (Http, Ajax)/http-01/src/components/Post/Post.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import './Post.css';
+import "./Post.css";
 
-const post = (props) => (
-    <article className="Post">
-        <h1>Title</h1>
-        <div className="Info">
-            <div className="Author">Author</div>
-        </div>
-    </article>
+const post = props => (
+  <article className="Post">
+    <h1>{props.title}</h1>
+    <div className="Info">
+      <div className="Author">{props.author}</div>
+    </div>
+  </article>
 );
 
 export default post;
--- Section 9 - Reaching out to the web (Http, Ajax)/http-01/src/containers/Blog/Blog.js
+++ Section 9 - Reaching out to the web (Http, Ajax)/http-01/src/containers/Blog/Blog.js
@@ -1,28 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
+      //   console.log(response);
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map(post => {
+        return {
+          ...post,
+          author: "Max"
+        };
+      });
+      this.setState({ posts: updatedPosts });
+    });
+  }
+
+  render() {
+    // console.log(this.state.posts);
+    const posts = this.state.posts.map(post => (
+      <Post title={post.title} key={post.id} author={post.author} />
+    ));
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
