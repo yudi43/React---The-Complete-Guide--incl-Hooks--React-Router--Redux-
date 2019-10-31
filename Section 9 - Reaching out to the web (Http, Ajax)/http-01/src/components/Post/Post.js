ba53d82125434b9f328cee6ad97e1464a5331b7b
Yudhishthir Singh <singh.yudi10@gmail.com>
2019-11-01 03:28:24 +0530

    added axios

--- Section 9 - Reaching out to the web (Http, Ajax)/http-01/src/components/Post/Post.js
+++ Section 9 - Reaching out to the web (Http, Ajax)/http-01/src/components/Post/Post.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 
-import classes from "./Post.module.css";
+import './Post.css';
 
-const post = props => (
-  <article className={classes.Post}>
-    <h1>Title</h1>
-    <div className="Info">
-      <div className="Author">Author</div>
-    </div>
-  </article>
+const post = (props) => (
+    <article className="Post">
+        <h1>Title</h1>
+        <div className="Info">
+            <div className="Author">Author</div>
+        </div>
+    </article>
 );
 
 export default post;
--- Section 9 - Reaching out to the web (Http, Ajax)/http-01/src/containers/Blog/Blog.js
+++ Section 9 - Reaching out to the web (Http, Ajax)/http-01/src/containers/Blog/Blog.js
@@ -1,28 +1,28 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import Post from "../../components/Post/Post";
-import FullPost from "../../components/FullPost/FullPost";
-import NewPost from "../../components/NewPost/NewPost";
-import classes from "./Blog.module.css";
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
 
 class Blog extends Component {
-  render() {
-    return (
-      <div>
-        <section className={classes.Posts}>
-          <Post />
-          <Post />
-          <Post />
-        </section>
-        <section>
-          <FullPost />
-        </section>
-        <section>
-          <NewPost />
-        </section>
-      </div>
-    );
-  }
+    render () {
+        return (
+            <div>
+                <section className="Posts">
+                    <Post />
+                    <Post />
+                    <Post />
+                </section>
+                <section>
+                    <FullPost />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
 }
 
 export default Blog;
