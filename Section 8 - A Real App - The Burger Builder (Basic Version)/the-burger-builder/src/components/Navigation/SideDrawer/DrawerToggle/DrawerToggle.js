7976ba8be07443568f46533c437755e0f250a9aa
Yudhishthir Singh <singh.yudi10@gmail.com>
2019-10-31 17:34:35 +0530

    app is ready, optimizations pending

--- Section 8 - A Real App - The Burger Builder (Basic Version)/the-burger-builder/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ Section 8 - A Real App - The Burger Builder (Basic Version)/the-burger-builder/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,7 +1,14 @@
 import React from "react";
+import classes from "./DrawerToggle.module.css";
 
 const drawerToggle = props => {
-  return <div onClick={props.clicked}>Menu</div>;
+  return (
+    <div className={classes.DrawerToggle} onClick={props.clicked}>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default drawerToggle;
