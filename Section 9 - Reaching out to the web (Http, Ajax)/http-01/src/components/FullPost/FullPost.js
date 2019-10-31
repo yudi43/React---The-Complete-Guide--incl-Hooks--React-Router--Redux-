ffefb7cf90ea8cffbe24da3acdd8b4d8041ddac6
Yudhishthir Singh <singh.yudi10@gmail.com>
2019-11-01 03:04:24 +0530

    working setup done

--- Section 9 - Reaching out to the web (Http, Ajax)/http-01/src/components/FullPost/FullPost.js
+++ Section 9 - Reaching out to the web (Http, Ajax)/http-01/src/components/FullPost/FullPost.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './FullPost.css';
+import classes from "./FullPost.module.css";
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
-
-        );
-        return post;
-    }
+  render() {
+    let post = <p>Please select a Post!</p>;
+    post = (
+      <div className={classes.FullPost}>
+        <h1>Title</h1>
+        <p>Content</p>
+        <div className={classes.Edit}>
+          <button className={classes.Delete}>Delete</button>
+        </div>
+      </div>
+    );
+    return post;
+  }
 }
 
 export default FullPost;
