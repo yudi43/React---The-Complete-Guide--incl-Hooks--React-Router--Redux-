e8100307368ffa5733a0b2e49c1791bcbf56b8f3
Yudhishthir Singh <singh.yudi10@gmail.com>
2019-11-03 01:58:49 +0530

    react-router and react-router-dom working

--- Section 11 - Multi-Page-Feeling in a Single-Page-App Routing/routing/src/containers/Blog/Blog.js
+++ Section 11 - Multi-Page-Feeling in a Single-Page-App Routing/routing/src/containers/Blog/Blog.js
@@ -56,7 +56,19 @@ class Blog extends Component {
     }
 
     return (
-      <div>
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/new-post">New Post</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
         <section className="Posts">{posts}</section>
         <section>
           <FullPost id={this.state.selectedPostId} />
