c660d939475bab1ad808d658cf10f67f17714563
Yudhishthir Singh <singh.yudi10@gmail.com>
2019-11-04 00:06:27 +0530

    React Suspense

--- Section 11 - Multi-Page-Feeling in a Single-Page-App Routing/routing--react-suspense/src/App.js
+++ Section 11 - Multi-Page-Feeling in a Single-Page-App Routing/routing--react-suspense/src/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter, Route, NavLink } from "react-router-dom";
 
-import Posts from './containers/Posts';
-import User from './containers/User';
-import Welcome from './containers/Welcome';
+// import Posts from './containers/Posts';
+import User from "./containers/User";
+import Welcome from "./containers/Welcome";
+
+const Posts = React.lazy(() => import("./containers/Posts"));
 
 class App extends Component {
   render() {
@@ -16,7 +18,17 @@ class App extends Component {
           </nav>
           <Route path="/" component={Welcome} exact />
           <Route path="/user" component={User} />
-          <Route path="/posts" component={Posts} />
+          {/* <Route path="/posts" component={Posts} /> */}
+          <Route
+            path="/posts"
+            render={() => {
+              return (
+                <React.Suspense fallback={<div>Loading...</div>}>
+                  <Posts />
+                </React.Suspense>
+              );
+            }}
+          />
         </React.Fragment>
       </BrowserRouter>
     );
