f616709c18fa881350152c2c8ea8710ca892bb82
Yudhishthir Singh <singh.yudi10@gmail.com>
2019-11-03 10:38:54 +0530

    conditional redirect

--- Section 11 - Multi-Page-Feeling in a Single-Page-App Routing/routing/src/containers/Blog/NewPost/NewPost.js
+++ Section 11 - Multi-Page-Feeling in a Single-Page-App Routing/routing/src/containers/Blog/NewPost/NewPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Redirect } from "react-router-dom";
 
 import "./NewPost.css";
 
@@ -7,7 +8,8 @@ class NewPost extends Component {
   state = {
     title: "",
     content: "",
-    author: "Max"
+    author: "Max",
+    submitted: false
   };
 
   componentDidMount() {
@@ -20,12 +22,20 @@ class NewPost extends Component {
       content: this.state.content,
       author: this.state.author
     };
-    axios.post("/posts", post).then(response => console.log(response));
+    axios.post("/posts", post).then(response => {
+      console.log(response);
+      this.setState({ submitted: true });
+    });
   };
 
   render() {
+    let redirect = null;
+    if (this.state.submitted) {
+      redirect = <Redirect to="/posts" />;
+    }
     return (
       <div className="NewPost">
+        {redirect}
         <h1>Add a Post</h1>
         <label>Title</label>
         <input
