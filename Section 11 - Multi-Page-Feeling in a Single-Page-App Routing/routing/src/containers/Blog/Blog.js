8b9623f33e43317577f02ba5bb297489640987b1
Yudhishthir Singh <singh.yudi10@gmail.com>
2019-11-03 07:58:24 +0530

    Redirecting from "/" to "/posts/"

--- Section 11 - Multi-Page-Feeling in a Single-Page-App Routing/routing/src/containers/Blog/Blog.js
+++ Section 11 - Multi-Page-Feeling in a Single-Page-App Routing/routing/src/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 // import axios from "axios";
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
-import { Route, NavLink, Switch } from "react-router-dom";
+import { Route, NavLink, Switch, Redirect } from "react-router-dom";
 
 import "./Blog.css";
 
@@ -44,6 +44,8 @@ class Blog extends Component {
         <Switch>
           <Route path="/new-post" component={NewPost} />
           <Route path="/posts" component={Posts} />
+          <Redirect from="/" to="/posts/" />
+          {/* <Route path="/" component={Posts} /> 1st way of redirecting */}
         </Switch>
       </div>
     );
