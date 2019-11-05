ad53b4ea8257d409693447ce1133f0cdb420f813
Yudhishthir Singh <singh.yudi10@gmail.com>
2019-11-05 20:05:52 +0530

    Fetched orders from firebase now rendering them

--- Section 12 - Adding Routing to our Burger Project/section12npx/src/containers/Orders/Orders.js
+++ Section 12 - Adding Routing to our Burger Project/section12npx/src/containers/Orders/Orders.js
@@ -1,7 +1,29 @@
 import React, { Component } from "react";
 import Order from "../../components/Order/Order";
-
+import axios from "../../axios-orders";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 export class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true
+  };
+  componentDidMount() {
+    axios
+      .get("/orders.json")
+      .then(res => {
+        // console.log(res);
+        const fetchedOrders = [];
+        for (let key in res.data) {
+          //   console.log(key);
+          fetchedOrders.push({ ...res.data[key], id: key });
+        }
+        console.log(fetchedOrders);
+        this.setState({ loading: false, orders: fetchedOrders });
+      })
+      .catch(err => {
+        this.setState({ loading: false });
+      });
+  }
   render() {
     return (
       <div>
@@ -12,4 +34,4 @@ export class Orders extends Component {
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
