a7c36f3b99d040081c23760af0dc47cacb528e22
Yudhishthir Singh <singh.yudi10@gmail.com>
2019-11-05 20:19:48 +0530

    displayed orders on orders route

--- Section 12 - Adding Routing to our Burger Project/section12npx/src/components/Order/Order.js
+++ Section 12 - Adding Routing to our Burger Project/section12npx/src/components/Order/Order.js
@@ -2,11 +2,34 @@ import React from "react";
 import classes from "./Order.module.css";
 
 const Order = props => {
+  const ingredients = [];
+  for (let ingredientName in props.ingredients) {
+    ingredients.push({
+      name: ingredientName,
+      amount: props.ingredients[ingredientName]
+    });
+  }
+  const ingredientOutput = ingredients.map(ig => {
+    return (
+      <span
+        style={{
+          textTransform: "capitalize",
+          display: "inline-block",
+          margin: "0 8px",
+          border: "1px solid #ccc",
+          padding: "5px"
+        }}
+        key={ig.name}
+      >
+        {ig.name} ({ig.amount})
+      </span>
+    );
+  });
   return (
     <div className={classes.Order}>
-      <p>Ingredients: </p>
+      <p>Ingredients: {ingredientOutput}</p>
       <p>
-        Price: <strong>$ 4.6</strong>
+        Price: <strong>$ {props.price.toFixed(2)}</strong>
       </p>
     </div>
   );
--- Section 12 - Adding Routing to our Burger Project/section12npx/src/containers/Orders/Orders.js
+++ Section 12 - Adding Routing to our Burger Project/section12npx/src/containers/Orders/Orders.js
@@ -27,8 +27,15 @@ export class Orders extends Component {
   render() {
     return (
       <div>
-        <Order />
-        <Order />
+        {this.state.orders.map(order => {
+          return (
+            <Order
+              key={order.id}
+              ingredients={order.ingredients}
+              price={+order.price}
+            />
+          );
+        })}
       </div>
     );
   }
