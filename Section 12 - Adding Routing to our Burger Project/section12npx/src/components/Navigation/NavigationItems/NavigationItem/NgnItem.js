f327df98ad1145b9f94824e7ed6a355d6eb919d8
Yudhishthir Singh <singh.yudi10@gmail.com>
2019-11-05 19:48:14 +0530

    fetching order from the server

--- Section 12 - Adding Routing to our Burger Project/section12npx/src/components/Navigation/NavigationItems/NavigationItem/NgnItem.js
+++ Section 12 - Adding Routing to our Burger Project/section12npx/src/components/Navigation/NavigationItems/NavigationItem/NgnItem.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import classes from "./NgnItem.module.css";
 
 const navigationItem = props => {
   return (
     <li className={classes.NavigationItem}>
-      <a href={props.link} className={props.active ? classes.active : null}>
+      <NavLink to={props.link} exact activeClassName={classes.active}>
         {props.children}
-      </a>
+      </NavLink>
     </li>
   );
 };
--- Section 12 - Adding Routing to our Burger Project/section12npx/src/components/Navigation/NavigationItems/NgnItems.js
+++ Section 12 - Adding Routing to our Burger Project/section12npx/src/components/Navigation/NavigationItems/NgnItems.js
@@ -5,10 +5,8 @@ import NavigationItem from "./NavigationItem/NgnItem";
 const navigationItems = () => {
   return (
     <ul className={classes.NavigationItems}>
-      <NavigationItem link="/" active>
-        BurgerBuilder
-      </NavigationItem>
-      <NavigationItem link="/">Checkout</NavigationItem>
+      <NavigationItem link="/">BurgerBuilder</NavigationItem>
+      <NavigationItem link="/orders">Orders</NavigationItem>
     </ul>
   );
 };
