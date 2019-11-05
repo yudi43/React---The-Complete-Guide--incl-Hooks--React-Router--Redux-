4345d17c79c9d326969278f7241494766095f908
Yudhishthir Singh <singh.yudi10@gmail.com>
2019-11-06 02:12:02 +0530

    added input component

---
+++ Section 13 - Forms and Form Validation/section13/src/components/UI/Input/Input.js
@@ -0,0 +1,24 @@
+import React from "react";
+import classes from "Input.module.css";
+
+const Input = props => {
+  let inputElement = null;
+  switch (props.inputType) {
+    case "input":
+      inputElement = <input className={classes.InputElement} {...props} />;
+      break;
+    case "textarea":
+      inputElement = <textarea className={classes.InputElement} {...props} />;
+      break;
+    default:
+      inputElement = <input className={classes.InputElement} {...props} />;
+  }
+
+  return (
+    <div className={classes.Input}>
+      <label className={classes.Label}>{props.label}</label>
+      {inputElement}
+    </div>
+  );
+};
+export default Input;
