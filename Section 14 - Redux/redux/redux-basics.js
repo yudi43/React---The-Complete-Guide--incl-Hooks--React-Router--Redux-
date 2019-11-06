74858b513b6ff8401acaaed3bb2a7adaa94ec9ca
Yudhishthir Singh <singh.yudi10@gmail.com>
2019-11-07 05:20:06 +0530

    Implementing redux on our project

--- Section 14 - Redux/redux/redux-basics.js
+++ Section 14 - Redux/redux/redux-basics.js
@@ -9,6 +9,18 @@ const initialState = {
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
+  if (action.type === "INC_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + 1
+    };
+  }
+  if (action.type === "ADD_COUNTER") {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    };
+  }
   return state;
 };
 
@@ -16,9 +28,12 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
+// Subscription
+store.subscribe(() => {
+  console.log("[SUBSCRIPTION]", store.getState());
+});
+
 // Dispatching Actions
 store.dispatch({ type: "INC_COUNTER" });
 store.dispatch({ type: "ADD_COUNTER", value: 10 });
 console.log(store.getState());
-
-// Subscription
