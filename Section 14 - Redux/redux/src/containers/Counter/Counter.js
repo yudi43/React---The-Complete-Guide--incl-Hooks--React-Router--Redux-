aaf479dad95fe2418be91303b747fd13a1087521
Yudhishthir Singh <singh.yudi10@gmail.com>
2019-11-07 06:18:18 +0530

    eject needed

--- Section 14 - Redux/redux/src/containers/Counter/Counter.js
+++ Section 14 - Redux/redux/src/containers/Counter/Counter.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import CounterControl from "../../components/CounterControl/CounterControl";
 import CounterOutput from "../../components/CounterOutput/CounterOutput";
@@ -36,7 +37,7 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <CounterOutput value={this.state.counter} />
+        <CounterOutput value={this.props.ctr} />
         <CounterControl
           label="Increment"
           clicked={() => this.counterChangedHandler("inc")}
@@ -58,4 +59,10 @@ class Counter extends Component {
   }
 }
 
-export default Counter;
+const mapStateToProps = state => {
+  return {
+    ctr: state.counter
+  };
+};
+
+export default connect(mapStateToProps)(Counter);
--- Section 14 - Redux/redux/src/index.js
+++ Section 14 - Redux/redux/src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from "redux";
+import reducer from "./store/reducer";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
