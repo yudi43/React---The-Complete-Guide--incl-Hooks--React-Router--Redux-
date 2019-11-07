f067d09c67d7d3f8fa3777c637191798f86d578b
Yudhishthir Singh <singh.yudi10@gmail.com>
2019-11-07 06:43:42 +0530

    eejct

--- Section 14 - Redux/redux--in-practice/src/index.js
+++ Section 14 - Redux/redux--in-practice/src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
