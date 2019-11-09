4fea6b47f520f58a51692f2be7dc849d5da5bb13
Yudhishthir Singh <singh.yudi10@gmail.com>
2019-11-10 02:33:31 +0530

    added middleware and redux devtools

--- Section 16 - Redux Advanced/section16/src/index.js
+++ Section 16 - Redux Advanced/section16/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 
 // importing the two reducers
@@ -15,7 +15,24 @@ const rootReducer = combineReducers({
   res: resultReducer
 });
 
-const store = createStore(rootReducer);
+const logger = store => {
+  return next => {
+    return action => {
+      console.log("[MIDDLEWARE] Dispatching", action);
+      const result = next(action);
+      console.log("[MIDDLEWARE] next state", store.getState());
+      return result;
+    };
+  };
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger))
+);
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
