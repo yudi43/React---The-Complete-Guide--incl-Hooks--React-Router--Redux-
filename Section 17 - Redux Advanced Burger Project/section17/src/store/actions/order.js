215a39ef15e733df51112c29d40e556431baa8b3
Yudhishthir Singh <singh.yudi10@gmail.com>
2019-11-11 01:12:16 +0530

    Refactored reducers

--- Section 17 - Redux Advanced Burger Project/section17/src/store/actions/order.js
+++ Section 17 - Redux Advanced Burger Project/section17/src/store/actions/order.js
@@ -61,6 +61,7 @@ export const fetchOrdersStart = () => {
 
 export const fetchOrders = () => {
   return dispatch => {
+    dispatch(fetchOrdersStart());
     axios
       .get("/orders.json")
       .then(res => {
