109f3faa5eb826a15b38abfa404cf64d1a6e96af
Yudhishthir Singh <singh.yudi10@gmail.com>
2019-11-14 01:00:33 +0530

    loggin out the user

--- Section 18 - Adding Authentication to our Burger Project/section18/src/store/actions/auth.js
+++ Section 18 - Adding Authentication to our Burger Project/section18/src/store/actions/auth.js
@@ -21,6 +21,19 @@ export const authFail = error => {
     error: error
   };
 };
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT
+  };
+};
+
+export const checkAuthTimeout = expirationTime => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
+  };
+};
 
 export const auth = (email, password, isSignUp) => {
   return dispatch => {
@@ -32,16 +45,17 @@ export const auth = (email, password, isSignUp) => {
       returnSecureToken: true
     };
     let url =
-      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=Your Api key";
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=YOUR API KEY";
     if (!isSignUp) {
       url =
-        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=Your Api key";
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=YOUR API KEY";
     }
     axios
       .post(url, authData)
       .then(response => {
         console.log(response);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
+        dispatch(checkAuthTimeout(response.data.expiresIn));
       })
       .catch(err => {
         console.log(err.response);
--- Section 18 - Adding Authentication to our Burger Project/section18/src/store/reducers/auth.js
+++ Section 18 - Adding Authentication to our Burger Project/section18/src/store/reducers/auth.js
@@ -25,6 +25,10 @@ const authFail = (state, action) => {
   return updateObject(state, { error: action.error, loading: false });
 };
 
+const authLogout = (state, action) => {
+  return updateObject(state, { token: null, userId: null });
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.AUTH_START:
@@ -33,6 +37,8 @@ const reducer = (state = initialState, action) => {
       return authSuccess(state, action);
     case actionTypes.AUTH_FAIL:
       return authFail(state, action);
+    case actionTypes.AUTH_LOGOUT:
+      return authLogout(state, action);
     default:
       return state;
   }
