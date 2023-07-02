00b4a05b24d9165de819e9410f16795abe3e0697
Yudi <singh.yudi10@gmail.com>
2023-07-02 19:40:17 +0530

    Add basic jsx to expMng

--- 2023/01-starting-setup/src/App.js
+++ 2023/01-starting-setup/src/App.js
@@ -1,7 +1,10 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem />
     </div>
   );
 }
---
+++ 2023/01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,9 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense Item</h2>
+    </div>
+  );
+}
+
+export default ExpenseItem;
