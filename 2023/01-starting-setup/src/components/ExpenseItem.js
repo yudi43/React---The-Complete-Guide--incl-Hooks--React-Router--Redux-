609c786173281c3416f82b1ba76986086e2bca2b
Yudi <singh.yudi10@gmail.com>
2023-07-03 08:58:21 +0530

    format the date

--- 2023/01-starting-setup/src/components/ExpenseItem.js
+++ 2023/01-starting-setup/src/components/ExpenseItem.js
@@ -1,12 +1,20 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>March 28 2023</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
-        <h2>Groceries</h2>
-        <div className="expense-item__price">$324.22</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
