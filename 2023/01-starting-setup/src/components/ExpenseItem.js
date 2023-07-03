34b97f97c9b010ee4f0e6a57dc25b11adf140439
Yudi <singh.yudi10@gmail.com>
2023-07-03 07:15:11 +0530

    add expense item and the related css

--- 2023/01-starting-setup/src/components/ExpenseItem.js
+++ 2023/01-starting-setup/src/components/ExpenseItem.js
@@ -1,7 +1,13 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
   return (
-    <div>
-      <h2>Expense Item</h2>
+    <div className="expense-item">
+      <div>March 28 2023</div>
+      <div className="expense-item__description">
+        <h2>Groceries</h2>
+        <div className="expense-item__price">$324.22</div>
+      </div>
     </div>
   );
 }
