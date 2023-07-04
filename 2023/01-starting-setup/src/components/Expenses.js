46ee6a646883dcb8fade40f4abe790d1b3e58fb7
Yudi <singh.yudi10@gmail.com>
2023-07-04 10:17:08 +0530

    Assignment submission, sepearting and styling the expanses component

---
+++ 2023/01-starting-setup/src/components/Expenses.js
@@ -0,0 +1,24 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+export default function Expenses(props) {
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      />
+      <ExpenseItem
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      />
+      <ExpenseItem
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+      />
+    </div>
+  );
+}
