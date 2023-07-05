48cd55db5759f7877ad57ef98d9a9089ce9cf99c
Yudi <singh.yudi10@gmail.com>
2023-07-05 10:59:29 +0530

    solution to assignment

--- 2023/02-React-key-concepts/src/App.js
+++ 2023/02-React-key-concepts/src/App.js
@@ -1,26 +1,26 @@
-import keyConceptsImage from './assets/images/key-concepts.png';
-import componentsImage from './assets/images/components.png';
-import stateImage from './assets/images/state.png';
-import eventsImage from './assets/images/events.png';
+import keyConceptsImage from "./assets/images/key-concepts.png";
+import componentsImage from "./assets/images/components.png";
+import stateImage from "./assets/images/state.png";
+import eventsImage from "./assets/images/events.png";
 
 const concepts = [
   {
-    title: 'Components',
+    title: "Components",
     image: componentsImage,
     description:
-      'Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.',
+      "Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.",
   },
   {
-    title: 'State',
+    title: "State",
     image: stateImage,
     description:
-      'State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.',
+      "State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.",
   },
   {
-    title: 'Events',
+    title: "Events",
     image: eventsImage,
     description:
-      'Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.',
+      "Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.",
   },
 ];
 
@@ -34,9 +34,19 @@ function App() {
       </header>
       <ul id="concepts">
         <li className="concept">
-          <img src="TODO: IMAGE" alt="TODO: TITLE" />
-          <h2>TODO: TITLE</h2>
-          <p>TODO: DESCRIPTION</p>
+          <img src={componentsImage} alt="Components image" />
+          <h2>{concepts[0].title}</h2>
+          <p>{concepts[0].description}</p>
+        </li>
+        <li className="concept">
+          <img src={stateImage} alt="state image" />
+          <h2>{concepts[1].title}</h2>
+          <p>{concepts[1].description}</p>
+        </li>
+        <li className="concept">
+          <img src={eventsImage} alt="events image" />
+          <h2>{concepts[2].title}</h2>
+          <p>{concepts[2].description}</p>
         </li>
       </ul>
     </div>
