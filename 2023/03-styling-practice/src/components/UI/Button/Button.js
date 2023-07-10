550c16631eb34685d08e9181b4ce378b4f9a2a49
Yudi <singh.yudi10@gmail.com>
2023-07-10 18:29:15 +0530

    convert normal css to styledcompnents

--- 2023/03-styling-practice/src/components/UI/Button/Button.js
+++ 2023/03-styling-practice/src/components/UI/Button/Button.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import styled from "styled-components";
 
-import './Button.css';
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+  .&:focus {
+    outline: none;
+  }
+
+  .&:hover,
+  .&:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`;
 
 export default Button;
