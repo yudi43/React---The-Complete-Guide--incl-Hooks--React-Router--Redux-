e0a47d543d96a4c247f59ac90661debedaeb1934
Yudi <singh.yudi10@gmail.com>
2023-07-10 10:55:22 +0530

    Conditional rendering, for add/remove expense

--- 2023/01-expense-manager/src/components/NewExpense/ExpenseForm.js
+++ 2023/01-expense-manager/src/components/NewExpense/ExpenseForm.js
@@ -69,6 +69,9 @@ function ExpenseForm(props) {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
--- 2023/01-expense-manager/src/components/NewExpense/NewExpense.js
+++ 2023/01-expense-manager/src/components/NewExpense/NewExpense.js
@@ -3,17 +3,34 @@ import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 function NewExpense(props) {
+  const [isEditing, setIsEditing] = useState(false);
   function saveExpenseDataHandler(enteredExpenseData) {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  }
+
+  function stopEditingHandler() {
+    setIsEditing(false);
   }
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={() => setIsEditing(true)}>Add new expense</button> //show the button is isEditing is false
+      )}
+
+      {
+        isEditing && (
+          <ExpenseForm
+            onSaveExpenseData={saveExpenseDataHandler}
+            onCancel={stopEditingHandler}
+          />
+        ) //show the form if isEditing if true
+      }
     </div>
   );
 }
