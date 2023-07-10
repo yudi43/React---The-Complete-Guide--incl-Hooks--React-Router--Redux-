bd7c116a2dd9871ddf5413911aacf246f3966c0a
Yudi <singh.yudi10@gmail.com>
2023-07-10 17:24:28 +0530

    Practice different types of styling in react

--- 2023/01-expense-manager/src/components/Chart/Chart.js
+++ 2023/01-expense-manager/src/components/Chart/Chart.js
@@ -5,6 +5,7 @@ import ChartBar from "./ChartBar";
 function Chart(props) {
   const dataPointsValues = props.dataPoints.map((dataPoint) => dataPoint.value);
   const totalMaximum = Math.max(...dataPointsValues);
+
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
