d91bf3664a8333de2ecea0b5746a48d2367d1a21
Yudi <singh.yudi10@gmail.com>
2023-07-15 19:01:28 +0530

    store form values in the form

--- 2023/04-investment-calculator/src/components/UserInputs/UserInputs.js
+++ 2023/04-investment-calculator/src/components/UserInputs/UserInputs.js
@@ -1,4 +1,15 @@
+import { useState } from "react";
+
+const initialUserInput = {
+  "current-savings": 10000,
+  "yearly-contribution": 1200,
+  "expected-return": 7,
+  duration: 10,
+};
+
 function UserInput() {
+  const [userInput, setUserInput] = useState(initialUserInput);
+
   const submitHandler = (event) => {
     event.preventDefault();
     // handle submit here...
@@ -6,18 +17,22 @@ function UserInput() {
   };
 
   function resetHandler() {
-    console.log("reset");
+    setUserInput(initialUserInput);
   }
 
   function inputChangeHandler(input, value) {
-    console.log(input, value);
+    setUserInput((prevState) => {
+      return { ...prevState, [input]: value };
+    });
   }
+
   return (
     <form onSubmit={submitHandler} className="form">
       <div className="input-group">
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
           <input
+            value={userInput["current-savings"]}
             onChange={(event) => {
               inputChangeHandler("current-savings", event.target.value);
             }}
@@ -28,6 +43,7 @@ function UserInput() {
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
           <input
+            value={userInput["yearly-contribution"]}
             onChange={(event) => {
               inputChangeHandler("yearly-contribution", event.target.value);
             }}
@@ -42,6 +58,7 @@ function UserInput() {
             Expected Interest (%, per year)
           </label>
           <input
+            value={userInput["expected-return"]}
             onChange={(event) => {
               inputChangeHandler("expected-return", event.target.value);
             }}
@@ -52,6 +69,7 @@ function UserInput() {
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
           <input
+            value={userInput["duration"]}
             onChange={(event) => {
               inputChangeHandler("duration", event.target.value);
             }}
