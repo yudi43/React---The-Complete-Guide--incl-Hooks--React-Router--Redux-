9664cf610e69f01b1badcb3e33e50ad74efe03af
Yudi <singh.yudi10@gmail.com>
2023-07-20 11:56:50 +0530

    Add available meals section, header, UI components and other jsx

--- 2023/07-food-ordering-app/src/App.js
+++ 2023/07-food-ordering-app/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </React.Fragment>
   );
 }
