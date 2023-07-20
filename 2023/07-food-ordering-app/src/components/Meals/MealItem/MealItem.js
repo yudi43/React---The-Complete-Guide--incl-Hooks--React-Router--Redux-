0a9fb84e4dca356be8d92f4b8a0b87cf73ea598c
Yudi <singh.yudi10@gmail.com>
2023-07-20 14:46:33 +0530

    add input form component

--- 2023/07-food-ordering-app/src/components/Meals/MealItem/MealItem.js
+++ 2023/07-food-ordering-app/src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
 
 export default function MealItem(props) {
   const price = `$${props.price.toFixed(2)}`;
@@ -10,7 +11,9 @@ export default function MealItem(props) {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   );
 }
---
+++ 2023/07-food-ordering-app/src/components/UI/Input.js
@@ -0,0 +1,10 @@
+import classes from "./Input.module.css";
+
+export default function Input(props) {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input {...props.input} />
+    </div>
+  );
+}
