895cb9d96d9a79f270177d09d8c010bb144d1d58
Yudi <singh.yudi10@gmail.com>
2023-07-24 19:59:22 +0530

    add useCounter custom hook using useState and useEffect

--- 2023/08-custom-hooks/src/components/ForwardCounter.js
+++ 2023/08-custom-hooks/src/components/ForwardCounter.js
@@ -1,17 +1,8 @@
-import { useState, useEffect } from 'react';
-
-import Card from './Card';
+import useCounter from "../hooks/use-counter";
+import Card from "./Card";
 
 const ForwardCounter = () => {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
+  const counter = useCounter();
 
   return <Card>{counter}</Card>;
 };
