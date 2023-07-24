4a2dd8fb4573e87e55063bccaa0d1526a1bd7db8
Yudi <singh.yudi10@gmail.com>
2023-07-24 20:04:29 +0530

    make useCounter more flexible by using parameters, and use it in Backward counter as well

--- 2023/08-custom-hooks/src/components/BackwardCounter.js
+++ 2023/08-custom-hooks/src/components/BackwardCounter.js
@@ -1,17 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import Card from './Card';
+import Card from "./Card";
+import useCounter from "../hooks/use-counter";
 
 const BackwardCounter = () => {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter - 1);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
+  const counter = useCounter(false);
 
   return <Card>{counter}</Card>;
 };
--- 2023/08-custom-hooks/src/hooks/use-counter.js
+++ 2023/08-custom-hooks/src/hooks/use-counter.js
@@ -1,15 +1,19 @@
 import { useState, useEffect } from "react";
 
-const useCounter = () => {
+const useCounter = (forwards = true) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      if (forwards) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [forwards]);
 
   return counter;
 };
