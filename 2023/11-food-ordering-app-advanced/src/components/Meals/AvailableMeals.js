aafd6f4126534e9c0ed54230fae3b42ac5296dde
Yudi <singh.yudi10@gmail.com>
2023-07-28 11:01:23 +0530

    add error handing and show loading when the data is loading

--- 2023/11-food-ordering-app-advanced/src/components/Meals/AvailableMeals.js
+++ 2023/11-food-ordering-app-advanced/src/components/Meals/AvailableMeals.js
@@ -5,16 +5,20 @@ import MealItem from "./MealItem/MealItem";
 
 export default function AvailableMeals(props) {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       const response = await fetch(
         "https://food-project-b7215-default-rtdb.firebaseio.com/meals.json"
       );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
       const responseData = await response.json();
-
       const loadedMeals = [];
-
       for (const key in responseData) {
         loadedMeals.push({
           id: key,
@@ -24,10 +28,37 @@ export default function AvailableMeals(props) {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
-    fetchData();
+    // try {
+    //   fetchData();
+    // } catch (error) {
+    //   setIsLoading(false);
+    //   setHttpError(error);
+    // }
+
+    fetchData().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error);
+    });
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
