87733b2aa72990ff8c8585d861f58a5d6b90e127
Yudi <singh.yudi10@gmail.com>
2023-08-02 09:57:43 +0530

    add toggle counter handler

--- 2023/12-Redux/src/components/Counter.js
+++ 2023/12-Redux/src/components/Counter.js
@@ -4,9 +4,8 @@ import classes from "./Counter.module.css";
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
-
-  const toggleCounterHandler = () => {};
 
   const incrementHandler = () => {
     dispatch({ type: "increment" });
@@ -17,10 +16,14 @@ const Counter = () => {
   const increaseHandler = () => {
     dispatch({ type: "increase", amount: 5 });
   };
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggleCounter" });
+  };
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <button onClick={incrementHandler}>Increment</button>
       <button onClick={decrementHandler}>Decrement</button>
       <button onClick={increaseHandler}>Increase by 5</button>
--- 2023/12-Redux/src/store/index.js
+++ 2023/12-Redux/src/store/index.js
@@ -1,21 +1,32 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "increase") {
     return {
       counter: state.counter + action.amount,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "toggleCounter") {
+    return {
+      counter: state.counter,
+      showCounter: !state.showCounter,
     };
   }
 
